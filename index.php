332f39d06a1a06b742195165805390a1e4895611
Boss4code <125689404+Boss4code@users.noreply.github.com>
2024-09-23 20:25:56 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>SUKELLE INVESTMENTS LIMITED</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css>
+     <link rel="stylesheet" href="css/stle.css">
+</head>
+<body> 
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Wellcome to Sukelle Investments Limited</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Company Profile <span class="sr-only">(current)</span></a>
+        <h1>Our Mission</h1>
+        <p>our mission is to provide our customer with the best business environment and better investment opportunities.</p>
+     </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+          Projects
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">next project</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Ongoing projects</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Completed Projects</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled">Contacts</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <img src="contect" class="d-block w-100" alt="v1">
+    </div>
+    <div class="carousel-item">
+      <img src="content" class="d-block w-100" alt="v2">
+    </div>
+  </div>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
